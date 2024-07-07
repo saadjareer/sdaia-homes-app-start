1b050ab3f13f39da90aca1348dd683a250c4fd07
Saad Jareer <sjareer.c@stc.com.sa>
2024-07-08 01:12:34 +0300

    add home-component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule} from "@angular/common";
+import { HousingLocationComponent} from "../housing-location/housing-location.component";
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
+  imports: [CommonModule, HousingLocationComponent],
+  template: '<section>' +
+      '<form>' +
+      '<input type="text" placeholder="Filter by city"/>' +
+      '<button type="button" class="primary">Search</button>' +
+      '</form>' +
+      '</section>' +
+      '<section class="results">' +
+      '<app-housing-location></app-housing-location>' +
+      '</section>',
+  standalone: true,
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
